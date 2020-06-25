f44d2cd477d63f5c6cf583d45070d55d0aa2da88
FrancoGT <francogt8000@gmail.com>
2020-06-25 07:57:34 +0200

    Mensaje de validacion

---
+++ assets/js/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+/* handling all the parsing */
+app.use(bodyParser.json());
+
+app.post('/', (req, res) => {
+    var email = req.body.email;
+    var amount = req.body.amount;
+
+    if (amount <= 1)
+    {
+        return_info = {};
+        return_info.error = true;
+        return_info.message = "The amount should be greater than 1";
+        return res.send(return_info);
+    }
+
+    res.send({'amount' : amount, 'email' : email});
+});
+
+app.listen(3000, () => {
+    console.log('server is running en port 3000');
+});
